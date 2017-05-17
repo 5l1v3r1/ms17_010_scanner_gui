509ecef58c9f0ae75c7cebe82cf1afcc93708dbd
liantian <liantian.me+code@gmail.com>
2017-05-17 21:55:15 +0800

    update

---
+++ checker/__init__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# __author__ = 'Liantian'
+# __email__ = "liantian.me+code@gmail.com"
+#
+# Copyright 2015-2016 liantian
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org>
+
+
+from jflyup import check as check_1
+from xunfeng import check as check_2
+from time import sleep
+
+
+def checker(ip, port=445, timeout=3):
+    result_1 = check_1(ip=ip, port=port, timeout=timeout)
+    sleep(timeout)
+    result_2 = check_2(ip=ip, port=port, timeout=timeout)
+
+    if result_1 or result_2:
+        return ip, True, "[+] is likely VULNERABLE to MS17-010"
+    elif (result_1 is False) or (result_2 is False):
+        return ip, False, "[-] stays in safety"
+    else:
+        return ip, None, "[*] cannot connect"
+
